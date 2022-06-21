12206ac34eb1747a80550084f89c13ff19dde757
Laurent Jouron <jouronlaurent@hotmail.com>
2022-06-21 13:20:42 +0200

    first ligne in js 21/06/2022

---
+++ JS/app.js
@@ -0,0 +1,30 @@
+class movie {
+    constructor(title, original_title, year, date_published , duration, description, long_description, avg_vote){
+      this.title = title;
+      this.original_title = original_title;
+      this.year = year;
+      this.date_published = date_published;
+      this.duration = duration;
+      this.description = description;
+      this.long_description = long_description;
+      this.avg_vote = avg_vote;
+    }
+  }
+
+  class movieActor {
+    constructor(movie, actor, position){
+      this.movie = movie;
+      this.actor = actor;
+      this.position = position;
+    }
+  }
+
+  class gender {
+    constructor(name, objects){
+      this.name = name;
+      this.objects = objects;
+    }
+  }
+
+
+
