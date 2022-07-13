84701380051877d6c6146deb8681c564bd2b4160
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-13 09:40:28 +0200

    first step javascript

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,9 @@
 const btnMoviePlay = document.querySelector(".box_movies--play-button");
-const btnLeftDirection = document.querySelector(".item_direction--left");
-const btnRightDirection = document.querySelector(".item_direction--right");
-const allItemsList = document.querySelectorAll('.item');
+const btnLeftDirection = document.querySelector(".container_list__direction--left");
+const btnRightDirection = document.querySelector(".container_list__direction--right");
+const allItemsList = document.querySelectorAll('.container_list--item');
 
-fetch('http://localhost:8000/api/v1/titles/')
+fetch('http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=')
     .then(reponse => reponse.json())
     .then(data => {
         console.log(data)
@@ -14,10 +14,14 @@ fetch('http://localhost:8000/api/v1/titles/')
     })
 
     btnLeftDirection.addEventListener('click', () => {
-        btnLeftDirection.getElementsByClassName("item_direction--left");
+         btnLeftDirection.getElementsByClassName("container_list__direction--left");
+
+        // btnLeftDirection.stopPropagation();
     })
 
     btnRightDirection.addEventListener('click', () => {
-        btnRightDirection.getElementsByClassName("item_direction--right");
+        btnRightDirection.getElementsByClassName("container_list__direction--right");
+    
+        // btnLeftDirection.stopPropagation();
     })
 
