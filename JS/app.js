98f36bac0b19d33693ea3786032782b91c7e7b90
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-15 09:50:33 +0200

    first fetch

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,23 @@
+const boxMovies = document.getElementById('box_movies');
 const categoryTitle = document.getElementsByClassName("category_title");
 const containerListItem = document.getElementsByClassName("container_list--item");
 const boxMoviesPlayButton = document.querySelector(".box_movies--play-button");
 const containerListDirectionLeft = document.querySelector(".container_list__direction--left");
 const containerListDirectionRight = document.querySelector(".container_list__direction--right");
 
-fetch(`http://localhost:8000/api/v1/titles/`)
-    .then(reponse => reponse.json())
-    .then(data => {
-        console.log(data.result)
-    })
+// http://localhost:8000/api/v1/titles/?imdb_score=9.3&genre=Comedy
+// http://localhost:8000/api/v1/titles/?imdb_score=9.4&genre=Drama
+
+fetch(`http://localhost:8000/api/v1/titles/?imdb_score=9.4&genre=Comedy`)
+    .then(response => {
+        if(response.ok) {
+            response.json().then(data => {
+                boxMovies.src = data.results[0].image_url;
+            console.log(boxMovies.src)})
+            } else {
+                document.getElementsByClassName("box_movies--title").innerHTML = "Error";
+            }
+        })
     
 
     boxMoviesPlayButton.addEventListener('click', () => {
