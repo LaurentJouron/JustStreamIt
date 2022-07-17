c8abab9d8fd6b6314edecdec39bd0a958420c3d8
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-17 12:23:35 +0200

    Progress

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,24 @@
-const boxMoviesPictures = document.getElementsByClassName('box_movies--pictures');
+const img = document.getElementsByClassName('best_movies__container--pictures');
 
 const containerListItem = document.getElementsByClassName("container_list--item");
-const boxMoviesPlayButton = document.querySelector(".box_movies--play-button");
+const bestMoviesPlayButton = document.querySelector(".best_movies__container__Items--play-button");
 const containerListDirectionLeft = document.querySelector(".container_list__direction--left");
 const containerListDirectionRight = document.querySelector(".container_list__direction--right");
 
-// http://localhost:8000/api/v1/titles/?imdb_score=9.3&genre=Comedy
-// http://localhost:8000/api/v1/titles/?imdb_score=9.4&genre=Drama
 
 fetch(`http://localhost:8000/api/v1/titles/?imdb_score=9.6&genre=Comedy`)
     .then(response => {
         if(response.ok) {
             response.json().then(data => {
-                boxMoviesPictures.src = data.results[0].image_url;
-            console.log(boxMoviesPictures.src)})
+                img.src = data.results[0].image_url;
+            console.log(img.src)})
             } else {
-                document.getElementsByClassName("category_title").innerHTML = "Error";
+                document.getElementsByClassName("best_movies__container__Items--title").innerHTML = "Error";
             }
         })
 
-    boxMoviesPlayButton.addEventListener('click', () => {
-        boxMoviesPlayButton.getElementsByClassName("box_movies--play-button");
+    bestMoviesPlayButton.addEventListener('click', () => {
+        bestMoviesPlayButton.getElementsByClassName("best_movies__container__Items--play-button");
     })
 
     containerListDirectionLeft.addEventListener('click', () => {
