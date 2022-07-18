10daa38963913bba149e476fb1dc6a03e4ce8b24
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-18 10:26:54 +0200

    Refactore and advance

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
-const img = document.getElementsByClassName('best_movies__container--pictures');
+const bestMoviesContainerPictures = document.getElementsByClassName('best_movies__container--pictures');
+const bestMoviesContainerItemsTitle = document.getElementsByClassName('best_movies__container__Items--title');
 
 const containerListItem = document.getElementsByClassName("container_list--item");
 const bestMoviesPlayButton = document.querySelector(".best_movies__container__Items--play-button");
@@ -10,12 +11,36 @@ fetch(`http://localhost:8000/api/v1/titles/?imdb_score=9.6&genre=Comedy`)
     .then(response => {
         if(response.ok) {
             response.json().then(data => {
-                img.src = data.results[0].image_url;
-            console.log(img.src)})
+                bestMoviesContainerPictures.src = data.results[0].image_url;
+                bestMoviesContainerItemsTitle.h1 = data.results[0].title;
+                console.log(data.results[0].title)
+            console.log(bestMoviesContainerPictures.src)})
             } else {
                 document.getElementsByClassName("best_movies__container__Items--title").innerHTML = "Error";
             }
         })
+
+// function bestMovies() {
+//     for(let i = 10; i === 0; i--) {
+//         fetch(`http://localhost:8000/api/v1/titles/?imdb_score=${i}&genre=Comedy`)
+//             .then(response => {
+//                 if(response.ok) {
+//                     response.json().then(data => {
+//                         bestMoviesContainerPictures.src = data.results[0].image_url;
+//                         bestMoviesContainerItemsTitle.h1 = data.results[0].title;
+//                     console.log(bestMoviesContainerItemsTitle.h1)
+//                     console.log(bestMoviesContainerPictures.src)})
+//                     } else {
+//                         document.getElementsByClassName("best_movies__container__Items--title").innerHTML = "Error";
+//                     }
+//                 });
+//             }
+//         }
+
+// genre : 'History', 'Drama', 'Documentary', 'Sport', 'Music', 'Animation', 'News',
+// 'Adventure', 'Adult', 'Sci-Fi', 'Family', 'Romance', 'Horror', 'Comedy', 'Biography',
+// 'Fantasy', 'Film-Noir', 'Crime', 'Action', 'Thriller', 'Western', 'Mystery', 'Reality-TV',
+// 'War', 'Musical',
 
     bestMoviesPlayButton.addEventListener('click', () => {
         bestMoviesPlayButton.getElementsByClassName("best_movies__container__Items--play-button");
