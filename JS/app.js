6aac69576b8dc51320b4da6ff52d060cf8fc2b1e
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-19 13:21:10 +0200

    refactore

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 const bestMoviesContainerPictures = document.getElementsByClassName('best_movies__container--pictures');
-const bestMoviesContainerItemsTitle = document.getElementsByClassName('best_movies__container__Items--title');
+const bestMoviesContainerItemsTitle = document.getElementsByClassName('h1.best_movies__container__Items--title');
 
+console.log(bestMoviesContainerItemsTitle)
 const containerListItem = document.getElementsByClassName("container_list--item");
 const bestMoviesPlayButton = document.querySelector(".best_movies__container__Items--play-button");
 const containerListDirectionLeft = document.querySelector(".container_list__direction--left");
@@ -15,23 +16,25 @@ function getRandomCategorie () {
  return genre[categorie];
 }
 
-function getMovieScore() {
-    for(i = Math.floor(20); i > 0; i = i - 0.1){
-        console.log(Number.parseFloat(i).toFixed(1))
-        }
-    }
+// function getMovieScore() {
+//     for(i = Math.floor(20); i > 0; i = i - 0.1){
+//         console.log(Number.parseFloat(i).toFixed(1))
+//         }
+//     }
 
 categorie = getRandomCategorie()
-score = getMovieScore()
+// score = getMovieScore()
 
-fetch(`http://localhost:8000/api/v1/titles/?imdb_score=5&genre=${categorie}`)
+fetch(`http://localhost:8000/api/v1/titles/?imdb_score=4&genre=${categorie}`)
     .then(response => {
         if(response.ok) {
             response.json().then(data => {
-                bestMoviesContainerPictures.src = data.results[0].image_url;
-                bestMoviesContainerItemsTitle.h1 = data.results[0].title;
-            console.log(data.results[0].title)
-            console.log(bestMoviesContainerPictures.src)})
+                let moviePicture = data.results[0].image_url;
+                let movieTitle = data.results[0].title;
+                bestMoviesContainerPictures.src = moviePicture;
+                bestMoviesContainerItemsTitle.textContent = movieTitle;
+            console.log(moviePicture)
+            console.log(movieTitle)})
             } else {
                 document.getElementsByClassName("best_movies__container__Items--title").innerHTML = "Error";
             }
