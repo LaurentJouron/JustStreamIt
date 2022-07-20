acb8fa1c4a1756c76faa59566acf9fa70134f2b5
Laurent Jouron <jouronlaurent@hotmail.com>
2022-07-20 10:06:23 +0200

    Pictures and title in the HTML but no CSS

--- JS/app.js
+++ JS/app.js
@@ -1,40 +1,21 @@
-const bestMoviesContainerPictures = document.getElementsByClassName('best_movies__container--pictures');
-const bestMoviesContainerItemsTitle = document.getElementsByClassName('h1.best_movies__container__Items--title');
+const bestMoviesContainerPictures = document.querySelector('.best_movies__container--pictures');
+const bestMoviesContainerItemsTitle = document.querySelector('.best_movies__container__Items--title');
 
-console.log(bestMoviesContainerItemsTitle)
-const containerListItem = document.getElementsByClassName("container_list--item");
+const containerListItem = document.querySelectorAll(".container_list--item");
+
 const bestMoviesPlayButton = document.querySelector(".best_movies__container__Items--play-button");
 const containerListDirectionLeft = document.querySelector(".container_list__direction--left");
 const containerListDirectionRight = document.querySelector(".container_list__direction--right");
 
-function getRandomCategorie () {
-    let genre = ['History', 'Drama', 'Documentary', 'Sport', 'Music', 'Animation', 'News',
-    'Adventure', 'Adult', 'Sci-Fi', 'Family', 'Romance', 'Horror', 'Comedy', 'Biography',
-    'Fantasy', 'Film-Noir', 'Crime', 'Action', 'Thriller', 'Western', 'Mystery', 'Reality-TV',
-    'War', 'Musical'];
-    categorie = Math.floor(Math.random() * (Math.ceil(0), Math.floor(genre.length)));
- return genre[categorie];
-}
-
-// function getMovieScore() {
-//     for(i = Math.floor(20); i > 0; i = i - 0.1){
-//         console.log(Number.parseFloat(i).toFixed(1))
-//         }
-//     }
-
-categorie = getRandomCategorie()
-// score = getMovieScore()
-
-fetch(`http://localhost:8000/api/v1/titles/?imdb_score=4&genre=${categorie}`)
+fetch(`http://localhost:8000/api/v1/titles/?imdb_score=9.6&genre=Comedy`)
     .then(response => {
         if(response.ok) {
             response.json().then(data => {
                 let moviePicture = data.results[0].image_url;
                 let movieTitle = data.results[0].title;
                 bestMoviesContainerPictures.src = moviePicture;
-                bestMoviesContainerItemsTitle.textContent = movieTitle;
-            console.log(moviePicture)
-            console.log(movieTitle)})
+                bestMoviesContainerItemsTitle.innerHTML = movieTitle;
+            })
             } else {
                 document.getElementsByClassName("best_movies__container__Items--title").innerHTML = "Error";
             }
@@ -57,3 +38,22 @@ fetch(`http://localhost:8000/api/v1/titles/?imdb_score=4&genre=${categorie}`)
         // containerListDirectionRight.stopPropagation();
     })
 
+
+
+// function getRandomCategorie () {
+//     let genre = ['History', 'Drama', 'Documentary', 'Sport', 'Music', 'Animation', 'News',
+//     'Adventure', 'Adult', 'Sci-Fi', 'Family', 'Romance', 'Horror', 'Comedy', 'Biography',
+//     'Fantasy', 'Film-Noir', 'Crime', 'Action', 'Thriller', 'Western', 'Mystery', 'Reality-TV',
+//     'War', 'Musical'];
+//     categorie = Math.floor(Math.random() * (Math.ceil(0), Math.floor(genre.length)));
+//  return genre[categorie];
+// }
+
+// function getMovieScore() {
+//     for(i = Math.floor(20); i > 0; i = i - 0.1){
+//         console.log(Number.parseFloat(i).toFixed(1))
+//         }
+//     }
+
+// categorie = getRandomCategorie()
+// score = getMovieScore()
