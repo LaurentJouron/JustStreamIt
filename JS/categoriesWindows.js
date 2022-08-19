fe5fcc17ab5b86be13602d64ebb543ffd162f54f
Laurent Jouron <jouronlaurent@hotmail.com>
2022-08-19 20:07:14 +0200

    New fetch

--- JS/categoriesWindows.js
+++ JS/categoriesWindows.js
@@ -1,7 +1,9 @@
 //////////////
 // REQUESTS //
 //////////////
-
+// http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=
+// http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=
+// GET /api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=9&imdb_score_max=10&title=&title_contains=&genre=&genre_contains=&sort_by=&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=
 
 ////////////////
 // CATEGORIES //
@@ -18,7 +20,6 @@ function categorieTitle(data, parent){
     newCategorieTitle.textContent = data.results[0].genres[0];
     parent.appendChild(newCategorieTitle);
 }
-
 
 /**
  * Function that allows to put the arrows in one direction or the other on each side of the images.
@@ -45,43 +46,54 @@ function sideArrows(parent, sense) {
  * @param {getElementById}
  * @returns {data}
  */
-async function movieCategorie(request, parent) {fetch(request)
-    .then(response => {if(response.ok) {response.json()
-        .then(data => {
-            // Function call to display the title above the category
-            categorieTitle(data, parent);
-    
-            // Div creation for container
-            let newContainerDiv = document.createElement('div');
-            newContainerDiv.className = 'container';
-            parent.appendChild(newContainerDiv);
-    
-            // Add previews arrows
-            sideArrows(newContainerDiv, '◀');
+ async function movieCategorie(request, parent) {
+    try {
+		const response = await fetch(request)
+			if(!response.ok) {
+				throw new Error(`Error: ${response.status}`)
+			}
+            if (response.ok) {response.json()
+                .then(data => {
+                    // Function call to display the title above the category
+                    categorieTitle(data, parent);
+                
+                    // Div creation for container
+                    let newContainerDiv = document.createElement('div');
+                    newContainerDiv.className = 'container';
+                    parent.appendChild(newContainerDiv);
             
-            for(let i = 0; i < 5; i++) {
-                let newPicture = document.createElement('img');
-                newPicture.className = 'container_list modal-trigger';      
-                newPicture.src = data.results[i].image_url;
-                newContainerDiv.appendChild(newPicture);
-            }
-
-        let nextPage = data.next
-            {fetch(nextPage)
-                .then(response => {if(response.ok) {response.json()
-                    .then(data => {
-                        for(let i = 0; i < 2; i++) {
-                            let newPicture = document.createElement('img');
-                            newPicture.className = 'container_list modal-trigger';      
-                            newPicture.src = data.results[i].image_url;
-                            newContainerDiv.appendChild(newPicture);
-                        }
-                        sideArrows(newContainerDiv, '▶');
-                    })
-                }})
-            }   
-        })
-    }})  
+                    // Add previews arrows
+                    sideArrows(newContainerDiv, '◀');
+                    
+                    // First loop that recovers the first 5 elements
+                    for(let i = 0; i < 5; i++) {
+                    let newPicture = document.createElement('img');
+                    newPicture.className = 'container_list modal-trigger';      
+                    newPicture.src = data.results[i].image_url;
+                    newContainerDiv.appendChild(newPicture);
+                }
+                    // Recovery of the next URL to make a loop for 2 elements recovery
+                    let nextPage = data.next
+                    {fetch(nextPage)
+                        .then(response => {if(response.ok) {response.json()
+                            .then(data => {
+                                for(let i = 0; i < 2; i++) {
+                                let newPicture = document.createElement('img');
+                                newPicture.className = 'container_list modal-trigger';      
+                                newPicture.src = data.results[i].image_url;
+                                newContainerDiv.appendChild(newPicture);
+                                }
+                            sideArrows(newContainerDiv, '▶');
+                        })
+                    }})
+                }
+            })
+        } else {
+            console.error('Retour du serveur : ', response.status)
+        }
+    } catch(error) {
+        errorMsg.textContent = `${error}`
+    }
 }
 
 const topRatedRequest = 'http://localhost:8000/api/v1/titles/?imdb_score=9.2'
@@ -99,111 +111,6 @@ movieCategorie(secondRequest, secondCategorie)
 const thirdRequest = 'http://localhost:8000/api/v1/titles/?genre=Adventure'
 const thirdCategorie = document.getElementById('third_categorie')
 movieCategorie(thirdRequest, thirdCategorie)
-
-
-
-//////////////////////////////////////
-// TEST CATEGORY FUNCTION GRAFIKART //
-//////////////////////////////////////
-
-
-// async function movieCategorie(request, parent) {
-// 	try{
-// 		const response = await fetch(request)
-// 			if(!response.ok) {
-// 				throw new Error(`Error: ${response.status}`)
-// 			}
-// 			if(!data.total) {
-// 				picturesList.textContent = "";
-// 				throw new Error("No results in our database. Please search more specifically.")
-// 			}
-//             if (response.ok) {response.json()
-//                 if (response.ok) {
-			
-//                 // Function call to display the title above the category
-//                     categorieTitle(data, parent);
-                
-//                 // Div creation for container
-//                     let newContainerDiv = document.createElement('div');
-//                     newContainerDiv.className = 'container';
-//                     parent.appendChild(newContainerDiv);
-                    
-//                 // Add previews arrows
-//                     sideArrows(newContainerDiv, '◀');
-                    
-//                     for(let i = 0; i < 5; i++) {
-//                         let newPicture = document.createElement('img');
-//                         newPicture.className = 'container_list modal-trigger';      
-//                         newPicture.src = data.results[i].image_url;
-//                         newContainerDiv.appendChild(newPicture);
-//                     }
-                    
-//                 let nextPage = data.next
-//                     try{
-//                         const response = await fetch(nextPage)
-//                         if(!response.ok) {
-//                             throw new Error(`Error: ${response.status}`)
-//                         }
-//                         if(!data.total) {
-//                             picturesList.textContent = "";
-//                             throw new Error("No results in our database. Please search more specifically.")
-//                         }
-//                         for(let i = 0; i < 2; i++) {
-//                             let newPicture = document.createElement('img');
-//                             newPicture.className = 'container_list modal-trigger';      
-//                             newPicture.src = data.results[i].image_url;
-//                             newContainerDiv.appendChild(newPicture);
-//                         }
-                        
-//                         }	
-//                         catch(error){
-//                             errorMsg.textContent = `${error}`
-//                         }
-//                     }	
-//             catch(error){
-//                 errorMsg.textContent = `${error}`
-//             }
-//         }
-//     }
-// }
-
-
-
-// Function for the movie categorie
-// async function movieCategorie(request, parent) {
-//     try {
-//         let response =  await fetch(request)
-//             if (response.ok) {response.json()
-//                 if (response.ok) {
-//                     let data = await response.json()
-
-//                     // Function call to display the title above the category
-//                     categorieTitle(data, parent);
-
-//                     // Div creation for container
-//                     let newContainerDiv = document.createElement('div');
-//                     newContainerDiv.className = 'container';
-//                     parent.appendChild(newContainerDiv);
-
-//                     // Add previews arrows
-//                     previewsArrows(newContainerDiv);
-
-//                     for(let i = 0; i < 5; i++) {
-//                         let newPicture = document.createElement('img');
-//                         newPicture.className = 'container_list modal-trigger';      
-//                         newPicture.src = data.results[i].image_url;
-//                         newContainerDiv.appendChild(newPicture);
-//                     }
-//                     nextArrows(newContainerDiv);
-//         } else {
-//             console.error('Retour du serveur : ', response.status)
-//         }
-//     }
-//     } catch (e) {
-//         console.error(e)
-//     }
-// }
-
 
 /////////////////////
 // RANDOM CATEGORY //
