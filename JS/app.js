734ee33bb2b4ab464314b8b8f59255e291aab5e9
Laurent Jouron <jouronlaurent@hotmail.com>
2022-08-30 13:33:53 +0200

    advancement

--- JS/app.js
+++ JS/app.js
@@ -1,75 +1,73 @@
-/**
- * Function that feeds the whole box at the top and represents the best film.
- * @param {ID from HTML file} idParent 
- * @param {Results from the fetch} data 
- */
- function bestMovie(idParent, data) {
-    const getMoviePicture = document.querySelector(`.movie_picture__${idParent}`)
-    const getMovieTitle = document.querySelector(`.movie_title__${idParent}`)
-    const playLink = document.querySelector(`play`)
-
-    getMovieTitle.textContent = data.results[0].title
-    getMoviePicture.src = data.results[0].image_url
-// data.results[0].imdb_url
+ function createElementWithClass (parentElement, curentElement, className) {
+    let element = document.createElement(curentElement)
+    element.className = className
+    parentElement.appendChild(element)
 }
-
-/**
- * This function allows to assign the title of each category, while assigning the name best rated first.
- * @param {ID from HTML file} idParent 
- * @param {Results from the fetch} data 
- */
-function categorieTitle (idParent, data) {
-    const getCategortieTitle = document.querySelector(`.categorie_title__${idParent}`)
-    if (idParent === "top_rated") {
-        getCategortieTitle.textContent = `Top rated`
-     } else {
-        getCategortieTitle.textContent = data.results[0].genres[0]
-    }
-}
-
-/**
- * @param {querySelector} 
- * @param {string} 
- */
- function createDivWithClass (parentElement, className) {
-    let div = document.createElement('div')
-    div.className = className
-    parentElement.appendChild(div)
-}
-
 
 async function getMovie(idParent, getCategorie) {
     const APIUrl = `http://localhost:8000/api/v1/titles/?`
     const slideParentElement = document.querySelector(`.slide__${idParent}`)
     const slideIndicators = document.querySelector(`.slide-indicators__${idParent}`)
-    let item = 0
-    let j = 1
     try {
-        const response = await fetch(APIUrl+getCategorie+`&page=${j}`)
+        const response = await fetch(APIUrl+getCategorie)
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`)
         }
         if (response.ok) {response.json()
             .then(data => {
                 if (idParent === `best_movie`) {
-                    bestMovie(idParent, data) 
+                    const getMoviePicture = document.querySelector(`.movie_picture__${idParent}`)
+                    const getMovieTitle = document.querySelector(`.movie_title__${idParent}`)
+                    // const playLink = document.querySelector(`play`)
+                
+                    getMovieTitle.textContent = data.results[0].title
+                    getMoviePicture.src = data.results[0].image_url
+                // data.results[0].imdb_url 
+                
                 } else {
-                    categorieTitle(idParent, data)
-                    while (item <= 7) {
+                    const getCategortieTitle = document.querySelector(`.categorie_title__${idParent}`)
+ 
+                    if (idParent === "top_rated") {
+                        getCategortieTitle.textContent = `Top rated`
+                     } else {
+                        getCategortieTitle.textContent = data.results[0].genres[0]
+                    }
+/// On itère sur les 5 films du paquet
+                    for(let i = 0; i < 5; i++) {
 
-                        createDivWithClass(slideParentElement, `slide--banner`)
+                        createElementWithClass(slideParentElement, `div`, `slide-banner`)
 
-                        let picture = document.createElement('img')
-                        picture.id = data.results[item].id
-                        picture.className = 'slide--item modal-trigger'
-                        picture.alt = data.results[item].title
-                        picture.src = data.results[item].image_url
-                        slideParentElement.appendChild(picture)
-                        
-                        createDivWithClass(slideIndicators, `slide-indicator`)
+                        let picture = document.createElement('img');
+                        picture.id = data.results[i].id;     
+                        picture.className = 'carousel--item modal-trigger';
+                        picture.alt = data.results[i].title;
+                        picture.src = data.results[i].image_url;
+                        picture.tabIndex = [i];
+                        slideParentElement.appendChild(picture);
 
-                        if (item === 4) {j++}
-                        item++
+                        createElementWithClass(slideIndicators, `div`, `slide-indicator`)
+                    }
+
+                    let nextPage = data.next
+                    {fetch(nextPage)
+                        .then(response => {if(response.ok) {response.json()
+                            .then(data => {
+                                for(let i = 0; i < 2; i++) {
+                                    createElementWithClass(slideParentElement, `div`, `slide-banner`)
+
+                                    let picture = document.createElement('img');
+                                    picture.id = data.results[i].id;     
+                                    picture.className = 'carousel--item modal-trigger';
+                                    picture.alt = data.results[i].title;
+                                    picture.src = data.results[i].image_url;
+                                    picture.tabIndex = [i];
+                                    slideParentElement.appendChild(picture);
+
+                                    createElementWithClass(slideIndicators, `div`, `slide-indicator`)
+                                }
+                            })
+
+                        }})
                     }
                 }
             })
@@ -85,7 +83,8 @@ const playButton = document.querySelector(".play");
     playButton.addEventListener('click', () => {
         playButton.getElementsByClassName("play");
         data.results[0].imdb_url
-})
+    }
+)
 
 getMovie(`best_movie`, `sort_by=-imdb_score`)
 getMovie(`top_rated`, `imdb_score_min=9&imdb_score_max=10`)
@@ -94,7 +93,76 @@ getMovie(`second_categorie`, `genre=Animation`)
 getMovie(`third_categorie`, `genre=War`)
 
 
+function autoplayCarousel(idParent) {
+    const carousel = document.getElementById(`carousel__${idParent}`)
+    const slideContainer = carousel.querySelector(`.slide_container__${idParent}`)
+    const slide = carousel.querySelector(`.slide`)
+    let slideWidth = slide.offsetWidth;
 
+    document.querySelector(`.preview__${idParent}`)
+        .addEventListener("click", () => navigate("backward"))
+
+    document.querySelector(`.next__${idParent}`)
+        .addEventListener("click", () => navigate("forward"))
+
+    document.querySelectorAll(".slide-indicator")
+        .forEach((dot, index) => {
+            dot.addEventListener("click", () => navigate(index))
+    })
+
+    // Add keyboard handlers
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'ArrowLeft') {
+            clearInterval(autoplay)
+            navigate("backward")
+        } else if (e.code === 'ArrowRight') {
+            clearInterval(autoplay)
+            navigate("forward")
+        }
+    })
+
+    // Add resize handler
+    window.addEventListener('resize', () => {
+        slideWidth = slide.offsetWidth;
+    })
+
+    const getNewScrollPosition = (arg) => {
+        const gap = 10
+        const maxScrollLeft = slideContainer.scrollWidth - slideWidth
+        if (arg === "forward") {
+            const x = slideContainer.scrollLeft + slideWidth + gap
+            return x <= maxScrollLeft ? x : 0
+        } else if (arg === "backward") {
+            const x = slideContainer.scrollLeft - slideWidth - gap
+            return x >= 0 ? x : maxScrollLeft
+        } else if (typeof arg === "number") {
+            const x = arg * (slideWidth + gap)
+            return x
+        }
+    }
+    const navigate = (arg) => {
+        slideContainer.scrollLeft = getNewScrollPosition(arg)
+    }
+
+    // Slide indicators
+    const slideObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const slideIndex = entry.target.dataset.slideindex;
+
+            }
+        })
+    }, 
+    { root: slideContainer, threshold: .1 })
+    document.querySelectorAll('.slide').forEach((slide) => {
+        slideObserver.observe(slide)
+    })
+}
+
+autoplayCarousel(`top_rated`)
+autoplayCarousel(`first_categorie`)
+autoplayCarousel(`second_categorie`)
+autoplayCarousel(`third_categorie`)
 
 // ///////////////////
 // // MODAL WINDOWS //
