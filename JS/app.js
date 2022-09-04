0a28a44f3ec090ba51fbfb01623831875ef96ad4
Laurent Jouron <jouronlaurent@hotmail.com>
2022-09-04 17:11:43 +0200

    JS advance

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,43 @@
+function modalWindow(idParent) {
+    const getMoviePicture = document.querySelector(`.movie_picture`);
+    const getMovieTitle = document.querySelector(`.movie_title`);
+    const getMovieGenres = document.querySelector(`.movie_genres`);
+    const getMovieDatePublished = document.querySelector(`.movie_date_published`);
+    const getMovieRated = document.querySelector(`.movie_rated`);
+    const getMovieScore = document.querySelector(`.movie_score`);
+    const getMovieDirectors = document.querySelector(`.movie_directors`);
+    const getMovieActors = document.querySelector(`.movie_actors`);
+    const getMovieDuration = document.querySelector(`.movie_duration`);
+    const getMovieOrigineCountries = document.querySelector(`.movie_origine_countries`);
+    const getMovieResultsOfBoxOffice = document.querySelector(`.movie_results_of_box_office`);
+    const getMovieDescription = document.querySelector(`.movie_description`);
+
+    getMoviePicture.src = data.results[0].image_url
+    getMovieTitle.textContent = "Title: " + data.results[0].title;
+    getMovieGenres.textContent = "Genres: " + data.results[0].genres;
+    getMovieDatePublished.textContent = "Date published: " + data.results[0].date_published;
+    getMovieRated.textContent = "Rated: " + data.results[0].rated;
+    getMovieScore.textContent = "Score: " + data.results[0].imdb_score;
+    getMovieDirectors.textContent = "Directors: " + data.results[0].directors;
+    getMovieActors.textContent = "Actors: " + data.results[0].actors;
+    getMovieDuration.textContent = "Duration: " + data.results[0].duration + " min";
+    getMovieOrigineCountries.textContent = "Countrie: " + data.results[0].countries;
+    // getMovieResultsOfBoxOffice.textContent = "Box Office: " + data.results[0].;
+    getMovieDescription.textContent = "Description: " + data.results[0].description;
+
+    idParent.appendChild(getMoviePicture);
+    idParent.appendChild(getMovieTitle);
+    idParent.appendChild(getMovieGenres);
+    idParent.appendChild(getMovieDatePublished);
+    idParent.appendChild(getMovieRated);
+    idParent.appendChild(getMovieScore);
+    idParent.appendChild(getMovieDirectors);
+    idParent.appendChild(getMovieActors);
+    idParent.appendChild(getMovieDuration);
+    idParent.appendChild(getMovieOrigineCountries);
+    idParent.appendChild(getMovieDescription);
+}
+
 /// Fecth qui fonctionne selon moi. Il me semble très chargée mais fonctionnelle.
 async function getMovie(idParent, getCategorie) {
     const APIUrl = `http://localhost:8000/api/v1/titles/?`
@@ -13,22 +53,47 @@ async function getMovie(idParent, getCategorie) {
                 if (idParent === `best_movie`) {
                     const getMoviePicture = document.querySelector(`.movie_picture__${idParent}`)
                     const getMovieTitle = document.querySelector(`.movie_title__${idParent}`)
-                    const getBestMovieModal = document.querySelector(`info`)
-                    const playLink = document.querySelector(`play_link`)
-                
+                    
+                    const modalPicture = document.getElementById('modal_picture');
+                    const modalTitle = document.getElementById('modal_title');
+                    const modalGenres = document.getElementById('modal_genres');
+                    const modalDatePublished = document.getElementById('modal_date_published');
+                    const modalRated = document.getElementById('rated');
+                    const modalScore = document.getElementById('modal_score');
+                    const modalDirectors = document.getElementById('modal_directors');
+                    const modalActors = document.getElementById('modal_actors');
+                    const modalDuration = document.getElementById('modal_duration');
+                    const modalOrigineCountries = document.getElementById('modal_origine_countries');
+                    const modalResultsOfBoxOffice = document.getElementById('modal_results_of_box_office');
+                    const modalDescription = document.getElementById('modal_description');
+               
                     getMovieTitle.textContent = data.results[0].title
                     getMoviePicture.src = data.results[0].image_url
-                    // playLink = data.results[0].url
-                    // getBestMovieModal =  data.results[0].imdb_url 
+                    modalPicture.src = data.results[0].image_url;
+                    modalTitle.textContent = "Title: " + data.results[0].title;
+                    modalGenres.textContent = "Genres: " + data.results[0].genres;
+                    modalDatePublished.textContent = "Date published: " + data.results[0].date_published;
+                    modalRated.textContent = "Rated: " + data.results[0].rated;
+                    modalScore.textContent = "Score: " + data.results[0].imdb_score;
+                    modalDirectors.textContent = "Directors: " + data.results[0].directors;
+                    modalActors.textContent = "Actors: " + data.results[0].actors;
+                    modalDuration.textContent = "Duration: " + data.results[0].duration + " min";
+                    modalOrigineCountries.textContent = "Countrie: " + data.results[0].countries;
+                    // modalResultsOfBoxOffice.textContent = "Box Office: " + data.results[0].;
+                    modalDescription.textContent = "Description: " + data.results[0].description;
+
 
                 } else {
 /// Condition pour nommer la première catégorie
                     const getCategortieTitle = document.querySelector(`.categorie_title__${idParent}`)
- 
-                    if (idParent === "top_rated") {
+                    if (idParent === `top_rated`) {
                         getCategortieTitle.textContent = `Top rated`
-                     } else {
-                        getCategortieTitle.textContent = data.results[0].genres[0]
+                     } else if (idParent === `first_categorie`) {
+                        getCategortieTitle.textContent = `Comedy`
+                    } else if (idParent === `second_categorie`){
+                        getCategortieTitle.textContent = `Animation`
+                    }  else if (idParent === `third_categorie`){
+                        getCategortieTitle.textContent = `Sport`
                     }
                     /// We read about the five films in the package
                     for(let i = 0; i < 5; i++) {
@@ -66,31 +131,22 @@ async function getMovie(idParent, getCategorie) {
     }
 }
 
-/// Bouton play sur lequel j'aimerais attribuer l'url du film
-const playButton = document.querySelector(".play_link");
-    playButton.addEventListener('click', () => {
-        playButton.getElementsByClassName("play_link");
-        data.results[0].imdb_url
-    }
-)
-
 /// Fonction pour gérer les images.
+function previewItem(idParent) {
+    const preview = document.querySelector(`.preview__${idParent}`);
+    preview.addEventListener('click', () => {
+        preview.querySelector(`.preview__top_rated`)
+        picturesGallery('')
+    })
+}
 
-// function previewItem(idParent) {
-//     const preview = document.querySelector(`.preview__${idParent}`);
-//     preview.addEventListener('click', () => {
-//         preview.querySelector(`.preview__top_rated`)
-//         picturesGallery('')
-//     })
-// }
-
-// function nextItem(idParent) {
-//     const next = document.querySelector(`.next__${idParent}`);
-//         next.addEventListener('click', () => {
-//             next.querySelector(`.next__top_rated`)
-//             picturesGallery(`-`)
-//     })
-// }
+function nextItem(idParent) {
+    const next = document.querySelector(`.next__${idParent}`);
+        next.addEventListener('click', () => {
+            next.querySelector(`.next__top_rated`)
+            picturesGallery(`-`)
+    })
+}
 
 
 /// Fonction de rotation des images
@@ -110,7 +166,6 @@ function picturesGallery(sign, idParent) {
 getMovie(`best_movie`, `sort_by=-imdb_score`)
 
 
-
 /// Appel de la fonction Fecth pour les films les mieux notés et attribution de la fonction de rotation aux boutons
 getMovie(`top_rated`, `imdb_score_min=9&imdb_score_max=10`)
 const previewTopRated = document.querySelector(`.preview__top_rated`);
@@ -127,7 +182,7 @@ nextTopRated.addEventListener('click', () => {
 
 
 /// Appel de la fonction Fecth pour les films de la première catégorie et attribution de la fonction de rotation aux boutons
-getMovie(`first_categorie`, `genre=Comedy`)
+getMovie(`first_categorie`, `genre=Comedy&sort_by=-imdb_score`)
 const previewFirstCategorie = document.querySelector(`.preview__first_categorie`);
 previewFirstCategorie.addEventListener('click', () => {
     previewFirstCategorie.querySelector(`.preview__first_categorie`)
@@ -142,7 +197,7 @@ nextFirstCategorie.addEventListener('click', () => {
 
 
 /// Appel de la fonction Fecth pour les films de la seconde catégorie et attribution de la fonction de rotation aux boutons
-getMovie(`second_categorie`, `genre=Animation`)
+getMovie(`second_categorie`, `genre=Animation&sort_by=-imdb_score`)
 const previewSecondCategorie = document.querySelector(`.preview__second_categorie`);
 previewSecondCategorie.addEventListener('click', () => {
     previewSecondCategorie.querySelector(`.preview__second_categorie`)
@@ -157,7 +212,7 @@ nextSecondCategorie.addEventListener('click', () => {
 
 
 /// Appel de la fonction Fecth pour les films de la troisième catégorie et attribution de la fonction de rotation aux boutons
-getMovie(`third_categorie`, `genre=War`)
+getMovie(`third_categorie`, `genre=sport&sort_by=-imdb_score`)
 const previewThirdCategorie = document.querySelector(`.preview__third_categorie`);
 previewThirdCategorie.addEventListener('click', () => {
     previewThirdCategorie.querySelector(`.preview__third_categorie`)
@@ -173,41 +228,6 @@ nextThirdCategorie.addEventListener('click', () => {
 // ///////////////////
 // // MODAL WINDOWS //
 // ///////////////////
-
-// Modal windows
-const modalPicture = document.getElementById('modal_picture');
-const modalTitle = document.getElementById('modal_title');
-const modalGenres = document.getElementById('modal_genres');
-const modalDatePublished = document.getElementById('modal_date_published');
-const modalRated = document.getElementById('rated');
-const modalScore = document.getElementById('modal_score');
-const modalDirectors = document.getElementById('modal_directors');
-const modalActors = document.getElementById('modal_actors');
-const modalDuration = document.getElementById('modal_duration');
-const modalOrigineCountries = document.getElementById('modal_origine_countries');
-const modalResultsOfBoxOffice = document.getElementById('modal_results_of_box_office');
-const modalDescription = document.getElementById('modal_description');
-
-function modalWindows(modalRequest) {fetch(modalRequest)
-        .then(response => {if(response.ok) {response.json()
-        .then(data => {
-            modalPicture.src = data.image_url;
-            modalTitle.textContent = "Title: " + data.title;
-            modalGenres.textContent = "Genres: " + data.genres;
-            modalDatePublished.textContent = "Date published: " + data.date_published;
-            modalRated.textContent = "Rated: " + data.rated;
-            modalScore.textContent = "Score: " + data.imdb_score;
-            modalDirectors.textContent = "Directors: " + data.directors;
-            modalActors.textContent = "Actors: " + data.actors;
-            modalDuration.textContent = "Duration: " + data.duration + " min";
-            modalOrigineCountries.textContent = "Countrie: " + data.countries;
-            // modalResultsOfBoxOffice.textContent = "Box Office: " + data.;
-            modalDescription.textContent = "Description: " + data.description;
-        })
-        }
-    })
-}
-
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 
