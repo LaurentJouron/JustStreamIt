fb42b3555b0b76d839e703e0ab2a59d5842c5bab
Laurent Jouron <jouronlaurent@hotmail.com>
2022-09-08 20:57:44 +0200

    refactore

--- JS/app.js
+++ JS/app.js
@@ -45,6 +45,7 @@ async function getMovieCategorie(idParent, getCategorie, categorieName) {
     const carousel = document.querySelector(`.carousel__${idParent}`)
     try {
         const response = await fetch(APIUrl+getCategorie)
+        // let _data = await response.json()
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`)
         }
@@ -104,191 +105,275 @@ getMovieCategorie(`first_categorie`, `?genre=Comedy&sort_by=-imdb_score`, `Comed
 getMovieCategorie(`second_categorie`, `?genre=Animation&sort_by=-imdb_score`, `Animation`)
 getMovieCategorie(`third_categorie`, `?genre=Sport&sort_by=-imdb_score`, `Sport`)
 
+//////////////////////
+// CAROUSEL VANILLA //
+//////////////////////
+// const carousel = document.querySelector('.carousel');
+// const carouselContent = document.querySelector('.carousel-content');
+// const img = document.querySelectorAll('.img');
+// let arrayOfSlides = Array.prototype.slice.call(img);
+// let carouselDisplaying;
+// let screenSize;
+// setScreenSize();
+// let lengthOfimg;
 
-//////////////
-// CAROUSEL //
-//////////////
-let angle = 0;
-function picturesGallery(sign, idParent) { 
-    spinner = document.querySelector(`.spinner__${idParent}`);
-    if (!sign) { angle = angle + 51.428 
-    } else { 
-        angle = angle - 51.428
-    }
-    spinner.setAttribute(`style`,`-webkit-transform: rotateY(${angle}deg)`); 
-        `-moz-transform: rotateY(${angle}deg)`; 
-        `transform: rotateY(${angle}deg)`;
-}
+// function addClone() {
+//     let lastSlide = carouselContent.lastElementChild.cloneNode(true);
+//     lastSlide.style.left = (-lengthOfSlide) + "px";
+//     carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
+// }
 
-const previewTopRated = document.querySelector(`.preview__top_rated`);
-previewTopRated.addEventListener('click', () => {
-    previewTopRated.querySelector(`.preview__top_rated`)
-    picturesGallery('', 'top_rated')
-})
+// function removeClone() {
+//   let firstSlide = carouselContent.firstElementChild;
+//   firstSlide.parentNode.removeChild(firstSlide);
+// }
+
+// function moveSlidesRight() {
+//   let img = document.querySelectorAll('.img');
+//   let slidesArray = Array.prototype.slice.call(img);
+//   let width = 0;
+
+//   slidesArray.forEach(function(el, i){
+//     el.style.left = width + "px";
+//     width += lengthOfSlide;
+//   });
+//   addClone();
+// }
+// moveSlidesRight();
+
+// function moveSlidesLeft() {
+//   let img = document.querySelectorAll('.img');
+//   let slidesArray = Array.prototype.slice.call(img);
+//   slidesArray = slidesArray.reverse();
+//   let maxWidth = (slidesArray.length - 1) * lengthOfSlide;
+
+//   slidesArray.forEach(function(el, i){
+//     maxWidth -= lengthOfSlide;
+//     el.style.left = maxWidth + "px";
+//   });
+// }
+
+// window.addEventListener('resize', setScreenSize);
+
+// function setScreenSize() {
+//   if ( window.innerWidth >= 500 ) {
+//     carouselDisplaying = 3;
+//   } else if ( window.innerWidth >= 300 ) {
+//     carouselDisplaying = 2;
+//   } else {
+//     carouselDisplaying = 1;
+//   }
+//   getScreenSize();
+// }
+
+// function getScreenSize() {
+//   let img = document.querySelectorAll('.img');
+//   let slidesArray = Array.prototype.slice.call(img);
+//   lengthOfSlide = ( carousel.offsetWidth  / carouselDisplaying );
+//   let initialWidth = -lengthOfSlide;
+//   slidesArray.forEach(function(el) {
+//     el.style.width = lengthOfSlide + "px";
+//     el.style.left = initialWidth + "px";
+//     initialWidth += lengthOfSlide;
+//   });
+// }
 
 
-const nextTopRated = document.querySelector(`.next__top_rated`);
-nextTopRated.addEventListener('click', () => {
-    nextTopRated.querySelector(`.next__top_rated`)
-    picturesGallery(`-`, `top_rated`)
-})
+// let rightNav = document.querySelector('.next');
+// rightNav.addEventListener('click', moveLeft);
+
+// let moving = true;
+// function moveRight() {
+//   if ( moving ) {
+//     moving = false;
+//     let lastSlide = carouselContent.lastElementChild;
+//     lastSlide.parentNode.removeChild(lastSlide);
+//     carouselContent.insertBefore(lastSlide, carouselContent.firstChild);
+//     removeClone();
+//     let firstSlide = carouselContent.firstElementChild;
+//     firstSlide.addEventListener('transitionend', activateAgain);
+//     moveSlidesRight();
+//   }
+// }
+
+// function activateAgain() {
+//   let firstSlide = carouselContent.firstElementChild;
+//   moving = true;
+//   firstSlide.removeEventListener('transitionend', activateAgain);
+// }
+
+// let leftNav = document.querySelector('.preview');
+// leftNav.addEventListener('click', moveRight);
+
+// function moveLeft() {
+//   if ( moving ) {
+//     moving = false;
+//     removeClone();
+//     let firstSlide = carouselContent.firstElementChild;
+//     firstSlide.addEventListener('transitionend', replaceToEnd);
+//     moveSlidesLeft();
+//   }
+// }
+
+// function replaceToEnd() {
+//   let firstSlide = carouselContent.firstElementChild;
+//   firstSlide.parentNode.removeChild(firstSlide);
+//   carouselContent.appendChild(firstSlide);
+//   firstSlide.style.left = ( (arrayOfSlides.length -1) * lengthOfSlide) + "px";
+//   addClone();
+//   moving = true;
+//   firstSlide.removeEventListener('transitionend', replaceToEnd);
+// }
+
+////////////////////////
+// CAROUSEL EN MANEGE //
+////////////////////////
+// let angle = 0;
+// function picturesGallery(sign, idParent) { 
+//     spinner = document.querySelector(`.spinner__${idParent}`);
+//     if (!sign) { angle = angle + 51.428 
+//     } else { 
+//         angle = angle - 51.428
+//     }
+//     spinner.setAttribute(`style`,`-webkit-transform: rotateY(${angle}deg)`); 
+//         `-moz-transform: rotateY(${angle}deg)`; 
+//         `transform: rotateY(${angle}deg)`;
+// }
+
+// const previewTopRated = document.querySelector(`.preview__top_rated`);
+// previewTopRated.addEventListener('click', () => {
+//     previewTopRated.querySelector(`.preview__top_rated`)
+//     picturesGallery('', 'top_rated')
+// })
 
 
-//////////////
+// const nextTopRated = document.querySelector(`.next__top_rated`);
+// nextTopRated.addEventListener('click', () => {
+//     nextTopRated.querySelector(`.next__top_rated`)
+//     picturesGallery(`-`, `top_rated`)
+// })
+
+
+/////////////////////////////
 // CAROUSEL TEST GRAFIKART //
-//////////////
-class Carousel {
-    constructor (element, options = {}) {
-        this.element = element
-        this.options = object.assign({}, {
-            slidesToScroll: 4,
-            slidesToVisible: 3,
-            loop: false
-        }, options)
+/////////////////////////////
+// class Carousel {
+//     constructor (element, options = {}) {
+//         this.element = element
+//         this.options = object.assign({}, {
+//             slidesToScroll: 4,
+//             slidesToVisible: 3,
+//             loop: false
+//         }, options)
         
-        let children = [].slice.call(element.children)
-        this.currentItem = 0
-        this.root = this.createDivWithClass('carousel')
-        this.container = this.createDivWithClass('carousel__panorama')
-        this.root.appendChild(this.container)
-        this.element.appendChild(this.root)
-        this.moveCallBacks = []
-        this.items = children.map((child) => {
-            let item = this.createDivWithClass('carousel__panorama--item')
-            item.appendChild(child)
-            this.container.appendChild(item)
-            return item
-        })
-        this.setStyle()
-        this.createNavigation()
-        this.moveCallBacks.forEach(callBack => callBack(0))
-    }
+//         let children = [].slice.call(element.children)
+//         this.currentItem = 0
+//         this.root = this.createDivWithClass('carousel')
+//         this.container = this.createDivWithClass('carousel__panorama')
+//         this.root.appendChild(this.container)
+//         this.element.appendChild(this.root)
+//         this.moveCallBacks = []
+//         this.items = children.map((child) => {
+//             let item = this.createDivWithClass('carousel__panorama--item')
+//             item.appendChild(child)
+//             this.container.appendChild(item)
+//             return item
+//         })
+//         this.setStyle()
+//         this.createNavigation()
+//         this.moveCallBacks.forEach(callBack => callBack(0))
+//     }
 
-    setStyle () {
-        let ratio = this.items.length / this.options.slidesToVisible
-        this.container.style.width = (ratio * 100) + "%"
-        this.items.forEach(item => item.style.width = ((100 / this.options.slidesToVisible) / ratio) + "%")   
-    }
+//     setStyle () {
+//         let ratio = this.items.length / this.options.slidesToVisible
+//         this.container.style.width = (ratio * 100) + "%"
+//         this.items.forEach(item => item.style.width = ((100 / this.options.slidesToVisible) / ratio) + "%")   
+//     }
 
-    createNavigation() {
-        let nextButton = this.createDivWithClass('carousel__next')
-        let prevButton = this.createDivWithClass('carousel__prev')
-        this.root.appendChild(nextButton)
-        this.root.appendChild(prevButton)
-        nextButton.addEventListener('click', this.next.bind(this))
-        prevButton.addEventListener('click', this.prev.bind(this))
+//     createNavigation() {
+//         let nextButton = this.createDivWithClass('carousel__next')
+//         let prevButton = this.createDivWithClass('carousel__prev')
+//         this.root.appendChild(nextButton)
+//         this.root.appendChild(prevButton)
+//         nextButton.addEventListener('click', this.next.bind(this))
+//         prevButton.addEventListener('click', this.prev.bind(this))
         
-        this.onMove(index => {
-            if(index === 0) {
-                prevButton.classList.add('carousel__prev--hidden')
-            } else {
-                prevButton.classList.remove('carousel__prev--hidden')
-            }
-            if (this.item[this.currentItem + this.options.slidesToVisible] === undefined){
-                nextButton.classList.add('carousel__next--hidden')
-            } else {
-                nextButton.classList.add('carousel__next--hidden')
-            }
-        })
-    }
+//         this.onMove(index => {
+//             if(index === 0) {
+//                 prevButton.classList.add('carousel__prev--hidden')
+//             } else {
+//                 prevButton.classList.remove('carousel__prev--hidden')
+//             }
+//             if (this.item[this.currentItem + this.options.slidesToVisible] === undefined){
+//                 nextButton.classList.add('carousel__next--hidden')
+//             } else {
+//                 nextButton.classList.add('carousel__next--hidden')
+//             }
+//         })
+//     }
 
-    next () {
-        this.gotToItem(this.currentItem + this.options.slidesToScroll)
+//     next () {
+//         this.gotToItem(this.currentItem + this.options.slidesToScroll)
 
-    }
+//     }
 
-    prev () {
-        this.gotToItem(this.currentItem - this.options.slidesToScroll)
-    }
+//     prev () {
+//         this.gotToItem(this.currentItem - this.options.slidesToScroll)
+//     }
 
-    gotToItem(index) {
-        if (index < 0) {
-            index = this.items.length - this.options.visible
-        } else if (index >= this.items.length || this.item[this.currentItem + this.options.slidesToVisible] === undefined) {
-            index = 0
-        }
+//     gotToItem(index) {
+//         if (index < 0) {
+//             index = this.items.length - this.options.visible
+//         } else if (index >= this.items.length || this.item[this.currentItem + this.options.slidesToVisible] === undefined) {
+//             index = 0
+//         }
 
-        let translateX = index * 100 / this.items.length
-        this.container.style.transfom = 'translate3d(' + translateX + ' %, 0, 0)'
-        this.currentItem = index
-        this.moveCallBacks.forEach(callBack => callBack(index))
-    }
+//         let translateX = index * 100 / this.items.length
+//         this.container.style.transfom = 'translate3d(' + translateX + ' %, 0, 0)'
+//         this.currentItem = index
+//         this.moveCallBacks.forEach(callBack => callBack(index))
+//     }
 
-    onMove(callBack) {
-        this.moveCallBacks.push(callBack)
-    }
+//     onMove(callBack) {
+//         this.moveCallBacks.push(callBack)
+//     }
 
-    createDivWithClass (className) {
-        let div = document.createElement('div')
-        div.setAttribute('class', className)
-        return div
-    }
-}
+//     createDivWithClass (className) {
+//         let div = document.createElement('div')
+//         div.setAttribute('class', className)
+//         return div
+//     }
+// }
     
-document.addEventListener('DOMContentLoaded', function () {
-    new Carousel(document.querySelector('.top_rated'), {
-        slidesToScroll: 3,
-        slidesToVisible: 4,
-        loop: false
-    })
+// document.addEventListener('DOMContentLoaded', function () {
+//     new Carousel(document.querySelector('.top_rated'), {
+//         slidesToScroll: 3,
+//         slidesToVisible: 4,
+//         loop: false
+//     })
 
-    new Carousel(document.querySelector('.first_categorie'), {
-        slidesToScroll: 3,
-        slidesToVisible: 4,
-        loop: false
-    })
-    new Carousel(document.querySelector('.second_categorie'), {
-        slidesToScroll: 3,
-        slidesToVisible: 4,
-        loop: false
-    })
-    new Carousel(document.querySelector('.third_categorie'), {
-        slidesToScroll: 3,
-        slidesToVisible: 4,
-        loop: false
-    })
-})
+//     new Carousel(document.querySelector('.first_categorie'), {
+//         slidesToScroll: 3,
+//         slidesToVisible: 4,
+//         loop: false
+//     })
+//     new Carousel(document.querySelector('.second_categorie'), {
+//         slidesToScroll: 3,
+//         slidesToVisible: 4,
+//         loop: false
+//     })
+//     new Carousel(document.querySelector('.third_categorie'), {
+//         slidesToScroll: 3,
+//         slidesToVisible: 4,
+//         loop: false
+//     })
+// })
 
 
 
 ///////////////////
 // MODAL WINDOWS //
 ///////////////////
-
-const getModalPicture = document.querySelector(`.modal_picture`);
-const getModalTitle = document.querySelector(`.modal_title`);
-const getModalGenres = document.querySelector(`.modal_genres`);
-const getModalDescription = document.querySelector(`.modal_description`);
-const getModalActors = document.querySelector(`.modal_actors`);
-const getModalDirectors = document.querySelector(`.modal_directors`);
-const getModalDuration = document.querySelector(`.modal_duration`);
-const getModalRated = document.querySelector(`.modal_rated`);
-const getModalScore = document.querySelector(`.modal_score`);
-const getModalDatePublished = document.querySelector(`.modal_date_published`);
-const getModalOrigineCountries = document.querySelector(`.modal_origine_countries`);
-const getModalResultsOfBoxOffice = document.querySelector(`.modal_results_of_box_office`);
-
-function getValueToModalElement(modalRequest) {fetch(modalRequest)
-    .then(response => {if(response.ok) {response.json()
-        .then(data => {
-            getModalPicture.src = data.image_url;
-            getModalTitle.textContent = "Title: " + data.title;
-            getModalGenres.textContent = "Genres: " + data.genres;
-            getModalDescription.textContent = "Description: " + data.description;
-            getModalActors.textContent = "Actors: " + data.actors;
-            getModalDirectors.textContent = "Directors: " + data.directors;
-            getModalDuration.textContent = "Duration: " + data.duration + " min";
-            getModalRated.textContent = "Rated: " + data.rated;
-            getModalScore.textContent = "Score: " + data.imdb_score;
-            getModalDatePublished.textContent = "Date published: " + data.date_published;
-            getModalOrigineCountries.textContent = "Countrie: " + data.countries;
-            // getModalResultsOfBoxOffice.textContent = "Box Office: " + data.;
-            })
-        }
-    })
-}
-
-
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 
@@ -342,6 +427,4 @@ async function getModalBox(movieURL) {
             errorMsg.textContent = `${error}`
     }
 }
-
-const modaltest = getModalBox(APIUrl+'8571428')
-console.log(modaltest)
+getModalBox(APIUrl+'8571428')
